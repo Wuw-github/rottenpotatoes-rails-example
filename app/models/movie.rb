f1461e0a6a8f73a99f56502110a3086385caf0e9
wuw-github <wuw1998@gmail.com>
2022-09-27 23:10:32 -0400

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,18 @@
 class Movie < ActiveRecord::Base
+
+    def Movie.all_ratings
+        return ['G', 'PG', 'PG-13', 'R']
+    end
+
+    def self.with_ratings(rating_list)
+        if rating_list == nil
+            return Movie.all
+        end
+        ret = []
+        rating_list.each do |rate, value|
+            ret += Movie.where(rating: rate)
+        end
+
+        return ret
+    end
 end
